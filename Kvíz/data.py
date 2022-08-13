3f9213528f0a50f8842c1d28b0f3764f4927c311
KerstynU <urbanovakristi@gmail.com>
2022-08-13 12:20:26 +0200

    third commit

---
+++ Kvíz/data.py
@@ -0,0 +1,8 @@
+question_data = [
+    {"text": "Python navrhl Guido van Rossum v roce 1991", "answer": "True"},
+    {"text": "Operační systém Linux byl založen Linusem Torvaldem", "answer": "True"},
+    {"text": "Operační systém Linux byl založen Linusem Torvaldem", "answer": "True"},
+    {"text": "HTML jazyk je také nazýván značkovacím jazykem", "answer": "True"},
+    {"text": "JavaScript patří mezi frontendové jazyky", "answer": "True"},
+    {"text": "CSS je programovací jazyk", "answer": "False"},
+]
